c377b9282666003fe46d252d20cd01da36ad9853
Valiulin.Sh.Sh <valiulin.sh.sh@gmail.com>
2023-09-24 20:49:57 +0300

    Добавлена стр. редактирования пользователя

--- src/Controller/ProfileController.php
+++ src/Controller/ProfileController.php
@@ -2,7 +2,10 @@
 
 namespace App\Controller;
 
+use App\Form\ProfileEditFormType;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\BrowserKit\Request;
+use Symfony\Component\HttpFoundation\Request as HttpFoundationRequest;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
@@ -15,6 +18,27 @@ class ProfileController extends AbstractController
     {
         return $this->render('main/profile/index.html.twig', [
             'controller_name' => 'ProfileController',
+        ]);
+    }
+
+    /**
+     * @Route("/profile/edit", name="app_profile_edit")
+     */
+    public function edit(HttpFoundationRequest $request): Response{
+        $user = $this->getUser();
+        $form = $this->createForm(ProfileEditFormType::class, $user);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $entityManager = $this->getDoctrine()->getManager();
+            $entityManager->persist($user);
+            $entityManager->flush();
+
+            return $this->redirectToRoute('app_profile');
+        }
+
+        return $this->render('main/profile/edit.html.twig', [
+            'form' => $form->createView()
         ]);
     }
 }
