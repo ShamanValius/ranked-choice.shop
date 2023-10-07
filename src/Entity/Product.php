7b48dab6860a9c91ef5f58512937cdada4c50589
Valiulin.Sh.Sh <valiulin.sh.sh@gmail.com>
2023-10-07 21:59:17 +0300

    Исправлена сущность Product

--- src/Entity/Product.php
+++ src/Entity/Product.php
@@ -14,177 +14,177 @@ use Doctrine\ORM\Mapping as ORM;
  */
 class Product
 {
-    /**
-     * @ORM\Id
-     * @ORM\GeneratedValue
-     * @ORM\Column(type="integer")
-     */
-    private $id;
+	/**
+	 * @ORM\Id
+	 * @ORM\GeneratedValue
+	 * @ORM\Column(type="integer")
+	 */
+	private $id;
 
-    /**
-     * @ORM\Column(type="string", length=255)
-     */
-    private $title;
+	/**
+	 * @ORM\Column(type="string", length=255)
+	 */
+	private $title;
 
-    /**
-     * @ORM\Column(type="decimal", precision=6, scale=2)
-     */
-    private $price;
+	/**
+	 * @ORM\Column(type="decimal", precision=6, scale=2)
+	 */
+	private $price;
 
-    /**
-     * @ORM\Column(type="integer")
-     */
-    private $quantity;
+	/**
+	 * @ORM\Column(type="integer")
+	 */
+	private $quantity;
 
-    /**
-     * @ORM\Column(type="datetime")
-     */
-    private $createdAt;
+	/**
+	 * @ORM\Column(type="datetime")
+	 */
+	private $createdAt;
 
-    /**
-     * @ORM\Column(type="text", nullable=true)
-     */
-    private $description;
+	/**
+	 * @ORM\Column(type="text", nullable=true)
+	 */
+	private $description;
 
-    /**
-     * @ORM\Column(type="boolean")
-     */
-    private $isPublished;
+	/**
+	 * @ORM\Column(type="boolean")
+	 */
+	private $isPublished;
 
-    /**
-     * @ORM\Column(type="boolean")
-     */
-    private $isDeleted;
+	/**
+	 * @ORM\Column(type="boolean")
+	 */
+	private $isDeleted;
 
-    /**
-     * @ORM\OneToMany(targetEntity=ProductImage::class, mappedBy="product", orphanRemoval=true)
-     */
-    private $productImages;
+	/**
+	 * @ORM\OneToMany(targetEntity=ProductImage::class, mappedBy="product", cascade={"persist"}, orphanRemoval=true)
+	 */
+	private $productImages;
 
-    public function __construct()
-    {
-        $this -> createdAt = new DateTimeImmutable();
-        $this -> isDeleted = false;
-        $this -> isPublished = false;
-        $this->productImages = new ArrayCollection();
-    }
+	public function __construct()
+	{
+		$this->createdAt = new DateTimeImmutable();
+		$this->isDeleted = false;
+		$this->isPublished = false;
+		$this->productImages = new ArrayCollection();
+	}
 
-    public function getId(): ?int
-    {
-        return $this->id;
-    }
+	public function getId(): ?int
+	{
+		return $this->id;
+	}
 
-    public function getTitle(): ?string
-    {
-        return $this->title;
-    }
+	public function getTitle(): ?string
+	{
+		return $this->title;
+	}
 
-    public function setTitle(string $title): self
-    {
-        $this->title = $title;
+	public function setTitle(string $title): self
+	{
+		$this->title = $title;
 
-        return $this;
-    }
+		return $this;
+	}
 
-    public function getPrice(): ?string
-    {
-        return $this->price;
-    }
+	public function getPrice(): ?string
+	{
+		return $this->price;
+	}
 
-    public function setPrice(string $price): self
-    {
-        $this->price = $price;
+	public function setPrice(string $price): self
+	{
+		$this->price = $price;
 
-        return $this;
-    }
+		return $this;
+	}
 
-    public function getQuantity(): ?int
-    {
-        return $this->quantity;
-    }
+	public function getQuantity(): ?int
+	{
+		return $this->quantity;
+	}
 
-    public function setQuantity(int $quantity): self
-    {
-        $this->quantity = $quantity;
+	public function setQuantity(int $quantity): self
+	{
+		$this->quantity = $quantity;
 
-        return $this;
-    }
+		return $this;
+	}
 
-    public function getCreatedAt(): ?\DateTimeInterface
-    {
-        return $this->createdAt;
-    }
+	public function getCreatedAt(): ?\DateTimeInterface
+	{
+		return $this->createdAt;
+	}
 
-    public function setCreatedAt(\DateTimeInterface $createdAt): self
-    {
-        $this->createdAt = $createdAt;
+	public function setCreatedAt(\DateTimeInterface $createdAt): self
+	{
+		$this->createdAt = $createdAt;
 
-        return $this;
-    }
+		return $this;
+	}
 
-    public function getDescription(): ?string
-    {
-        return $this->description;
-    }
+	public function getDescription(): ?string
+	{
+		return $this->description;
+	}
 
-    public function setDescription(?string $description): self
-    {
-        $this->description = $description;
+	public function setDescription(?string $description): self
+	{
+		$this->description = $description;
 
-        return $this;
-    }
+		return $this;
+	}
 
-    public function isIsPublished(): ?bool
-    {
-        return $this->isPublished;
-    }
+	public function getIsPublished(): ?bool
+	{
+		return $this->isPublished;
+	}
 
-    public function setIsPublished(bool $isPublished): self
-    {
-        $this->isPublished = $isPublished;
+	public function setIsPublished(bool $isPublished): self
+	{
+		$this->isPublished = $isPublished;
 
-        return $this;
-    }
+		return $this;
+	}
 
-    public function isIsDeleted(): ?bool
-    {
-        return $this->isDeleted;
-    }
+	public function getIsDeleted(): ?bool
+	{
+		return $this->isDeleted;
+	}
 
-    public function setIsDeleted(bool $isDeleted): self
-    {
-        $this->isDeleted = $isDeleted;
+	public function setIsDeleted(bool $isDeleted): self
+	{
+		$this->isDeleted = $isDeleted;
 
-        return $this;
-    }
+		return $this;
+	}
 
-    /**
-     * @return Collection<int, ProductImage>
-     */
-    public function getProductImages(): Collection
-    {
-        return $this->productImages;
-    }
+	/**
+	 * @return Collection<int, ProductImage>
+	 */
+	public function getProductImages(): Collection
+	{
+		return $this->productImages;
+	}
 
-    public function addProductImage(ProductImage $productImage): self
-    {
-        if (!$this->productImages->contains($productImage)) {
-            $this->productImages[] = $productImage;
-            $productImage->setProduct($this);
-        }
+	public function addProductImage(ProductImage $productImage): self
+	{
+		if (!$this->productImages->contains($productImage)) {
+			$this->productImages[] = $productImage;
+			$productImage->setProduct($this);
+		}
 
-        return $this;
-    }
+		return $this;
+	}
 
-    public function removeProductImage(ProductImage $productImage): self
-    {
-        if ($this->productImages->removeElement($productImage)) {
-            // set the owning side to null (unless already changed)
-            if ($productImage->getProduct() === $this) {
-                $productImage->setProduct(null);
-            }
-        }
+	public function removeProductImage(ProductImage $productImage): self
+	{
+		if ($this->productImages->removeElement($productImage)) {
+			// set the owning side to null (unless already changed)
+			if ($productImage->getProduct() === $this) {
+				$productImage->setProduct(null);
+			}
+		}
 
-        return $this;
-    }
+		return $this;
+	}
 }
