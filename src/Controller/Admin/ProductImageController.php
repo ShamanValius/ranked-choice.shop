f89dd40ae78f8db657e22db6a61669fce001ebf4
Valiulin.Sh.Sh <valiulin.sh.sh@gmail.com>
2023-10-08 13:59:48 +0500

    Дабавлены Controller для Product и ProductImage

---
+++ src/Controller/Admin/ProductImageController.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace App\Controller\Admin;
+
+use App\Utils\Manager\ProductImageManager;
+use App\Utils\Manager\ProductManager;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+use App\Form\Handler\ProductFormHandler;
+
+/**
+ * @Route("/admin/product-image", name="admin_product_image_")
+ */
+class ProductImageController extends AbstractController
+{
+	/**
+	 * @Route("/delete/{id}", name="delete")
+	 */
+	public function delete(ProductFormHandler $productFormHandler, ProductManager $productManager, ProductImageManager $productImageManager, int $id = null): Response
+	{
+		$productImage = $productFormHandler->getProductImage($id);
+		if (!$productImage) {
+			return $this->redirectToRoute('admin_product_list');
+		}
+
+		$product = $productImage->getProduct();
+
+		$productImagesDir = $productManager->getProductImagesDir($product);
+		$productImageManager->removeImageFromProduct($productImage, $productImagesDir);
+
+		return $this->redirectToRoute('admin_product_edit', [
+			'id' => $product->getId()
+		]);
+	}
+}
