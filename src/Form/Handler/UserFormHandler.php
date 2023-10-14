986926a15e44f82c132af15610618e9b361d5a10
Valiulin.Sh.Sh <valiulin.sh.sh@gmail.com>
2023-10-14 18:55:30 +0500

    Добавлен раздел управления пользователями

---
+++ src/Form/Handler/UserFormHandler.php
@@ -0,0 +1,64 @@
+<?php
+
+namespace App\Form\Handler;
+
+use App\Entity\User;
+use App\Utils\Manager\UserManager;
+use Symfony\Component\Form\Form;
+use Symfony\Component\Security\Core\Encoder\UserPasswordEncoderInterface;
+
+class UserFormHandler
+{
+    /**
+     * @var UserManager
+     */
+    private $userManager;
+
+    /**
+     * @var UserPasswordEncoderInterface
+     */
+    private $passwordEncoder;
+
+    public function __construct(UserManager $userManager, UserPasswordEncoderInterface $passwordEncoder)
+    {
+        $this->userManager = $userManager;
+        $this->passwordEncoder = $passwordEncoder;
+    }
+
+    public function getUser(?int $idUser)
+    {
+        if ($idUser) {
+            $user = $this->userManager->getRepository()->find($idUser);
+        } else {
+            $user = new User();
+        }
+
+        return $user;
+    }
+
+    /**
+     * @param Form $form
+     * @return User|null
+     */
+    public function processEditForm(Form $form)
+    {
+        $plainPassword = $form->get('plainPassword')->getData();
+        $newEmail = $form->get('newEmail')->getData();
+
+        /** @var User $user */
+        $user = $form->getData();
+
+        if (!$user->getId()) {
+            $user->setEmail($newEmail);
+        }
+
+        if ($plainPassword) {
+            $encodedPassword = $this->passwordEncoder->encodePassword($user, $plainPassword);
+            $user->setPassword($encodedPassword);
+        }
+
+        $this->userManager->save($user);
+
+        return $user;
+    }
+}
