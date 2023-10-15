d6f2d4ed1c4d661c9c37f0631c49788d7bdfb373
Valiulin.Sh.Sh <valiulin.sh.sh@gmail.com>
2023-10-15 23:52:53 +0500

    Добавлены начальные настройки ApiPlatform

---
+++ src/Utils/ApiPlatform/Extension/FilterProductQueryExtension.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace App\Utils\ApiPlatform\Extension;
+
+use ApiPlatform\Doctrine\Orm\Extension\QueryCollectionExtensionInterface;
+use ApiPlatform\Doctrine\Orm\Extension\QueryItemExtensionInterface;
+use ApiPlatform\Metadata\Operation;
+use ApiPlatform\Doctrine\Orm\Util\QueryNameGeneratorInterface;
+use App\Entity\Product;
+use Doctrine\ORM\QueryBuilder;
+
+class FilterProductQueryExtension implements QueryCollectionExtensionInterface, QueryItemExtensionInterface
+{
+    public function applyToCollection(QueryBuilder $queryBuilder, QueryNameGeneratorInterface $queryNameGenerator, string $resourceClass, Operation $operationName = null, array $context = []) : void
+    {
+        $this->andWhere($queryBuilder, $resourceClass);
+    }
+
+    public function applyToItem(QueryBuilder $queryBuilder, QueryNameGeneratorInterface $queryNameGenerator, string $resourceClass, array $identifiers, Operation $operation = null, array $context = []): void
+    {
+        $this->andWhere($queryBuilder, $resourceClass);
+    }
+
+    private function andWhere(QueryBuilder $queryBuilder, string $resourceClass): void
+    {
+        if (Product::class !== $resourceClass) {
+            return;
+        }
+
+        $rootAlias = $queryBuilder->getRootAliases()[0];
+
+        $queryBuilder->andWhere(
+            sprintf("%s.isDeleted = '0'", $rootAlias)
+        );
+    }
+}
